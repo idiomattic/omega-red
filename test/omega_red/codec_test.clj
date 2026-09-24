e1775acf8245387d328a383c90e4b3ed73ee607d
Matthew Lese <matthewjlese@gmail.com>
2026-09-24 11:06:44 -0700

    removes unit tests in codec ns

--- test/omega_red/codec_test.clj
+++ test/omega_red/codec_test.clj
@@ -1,7 +1,6 @@
 (ns omega-red.codec-test
   (:require [clojure.test :refer [deftest testing is]]
-            [omega-red.codec :as codec])
-  (:import [redis.clients.jedis.util KeyValue]))
+            [omega-red.codec :as codec]))
 
 (deftest key-serialization-test
   (testing "this will blow up"
@@ -30,27 +29,3 @@
 
   (testing "special case: things like HSET or SET return a number"
     (is (= 1 (codec/deserialize 1)))))
-
-(defn- ->bytes [s]
-  (String/.getBytes ^String s "UTF-8"))
-
-(defn- array-list [& items]
-  (java.util.ArrayList. ^java.util.Collection (vec items)))
-
-(deftest resp3-map-reply-deserialization-test
-  (testing "a list of KeyValue pairs (RESP3 map reply, e.g. HGETALL) flattens to [field val field val ...]"
-    (is (= ["a" "1" "b" "2"]
-           (codec/deserialize (array-list (KeyValue. (->bytes "a") (->bytes "1"))
-                                          (KeyValue. (->bytes "b") (->bytes "2")))))))
-
-  (testing "KeyValue values holding Clojure data are unserialized"
-    (is (= ["a" {:foo #{1 2}}]
-           (codec/deserialize (array-list (KeyValue. (->bytes "a") (->bytes (codec/serialize {:foo #{1 2}}))))))))
-
-  (testing "lists without KeyValue items keep their shape"
-    (is (= ["x" ["y" "z"]]
-           (codec/deserialize (array-list (->bytes "x")
-                                          (array-list (->bytes "y") (->bytes "z")))))))
-
-  (testing "an empty list stays an empty vector"
-    (is (= [] (codec/deserialize (array-list))))))
